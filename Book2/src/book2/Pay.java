c2ab859967f3a7cf31efd9f090023d1f89c98ec4
gejurier <julianariera1998@gmail.com>
2019-11-19 13:25:13 -0500

    Agregue la interface pay

---
+++ Book2/src/book2/Pay.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book2;
+
+/**
+ *
+ * @author CltControl
+ */
+public interface Pay {
+
+   void pay(String method, Book book);
+   void pay_card(String card, Book book);
+   public void pay_cash(String money, Book book);
+}
